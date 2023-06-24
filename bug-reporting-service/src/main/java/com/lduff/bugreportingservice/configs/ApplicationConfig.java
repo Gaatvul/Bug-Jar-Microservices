9bc5ede4661fb95d7fa52be7be015fadde076d5c
gaatvul <kungfuscience@hotmail.com>
2023-06-24 17:22:00 +0200

    Added Spring bean for bugReportService

--- bug-reporting-service/src/main/java/com/lduff/bugreportingservice/configs/ApplicationConfig.java
+++ bug-reporting-service/src/main/java/com/lduff/bugreportingservice/configs/ApplicationConfig.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.lduff.bugreportingservice.repositories.BugReportRepository;
+import com.lduff.bugreportingservice.services.BugReportService;
 
 @ComponentScan("com.lduff.bugreportingservice")
 @Configuration
 public class ApplicationConfig {
+
+    @Bean(name = "bugReportService")
+    public BugReportService createBugReportService() {
+        return new BugReportService();
+    }
 
     @Bean(name = "bugReportRepository")
     public BugReportRepository createBugReportRepository() {
