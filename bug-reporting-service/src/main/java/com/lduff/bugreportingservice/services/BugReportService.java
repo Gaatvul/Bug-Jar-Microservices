c0837b7edb696bc0ae16864007d299096e54063c
gaatvul <kungfuscience@hotmail.com>
2023-06-24 17:22:59 +0200

    Added BugReportRepository as an autowired bean

--- bug-reporting-service/src/main/java/com/lduff/bugreportingservice/services/BugReportService.java
+++ bug-reporting-service/src/main/java/com/lduff/bugreportingservice/services/BugReportService.java
@@ -2,16 +2,15 @@ package com.lduff.bugreportingservice.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.lduff.bugreportingservice.models.BugReport;
 import com.lduff.bugreportingservice.repositories.BugReportRepository;
 
 public class BugReportService {
 
-    private final BugReportRepository bugReportRepository;
-
-    BugReportService(BugReportRepository bugReportRepository) {
-        this.bugReportRepository = bugReportRepository;
-    }
+    @Autowired
+    BugReportRepository bugReportRepository;
 
     public List<BugReport> getAllBugReports() {
         return bugReportRepository.getAllBugReports();
