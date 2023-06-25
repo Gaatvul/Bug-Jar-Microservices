f094684f835def7b98d208203c38e069c82ff218
gaatvul <kungfuscience@hotmail.com>
2023-06-25 18:41:00 +0200

    Extracted bug report controller to URI. Filled in bug report instance fields. Added test for creating a new bug report.

--- bug-reporting-service/src/test/java/com/lduff/bugreportingservice/controllers/BugReportControllerTest.java
+++ bug-reporting-service/src/test/java/com/lduff/bugreportingservice/controllers/BugReportControllerTest.java
@@ -2,16 +2,21 @@ package com.lduff.bugreportingservice.controllers;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -30,17 +35,34 @@ public class BugReportControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private List<BugReport> bugReports;
+
+    @Value("${URI.bug-report-controller}")
+    private String BUG_REPORT_CONTROLLER_URI;
+
     @Test
     void getAllBugReportsShouldReturnListOfBugReports() throws Exception {
-
-        List<BugReport> bugReports = Arrays.asList(
-                new BugReport("title1", "description1", null, null, null, null, null, null, null),
-                new BugReport("title2", "description2", null, null, null, null, null, null, null));
+        bugReports = Arrays.asList(
+                new BugReport("title1", "description1", "status1", "severity1", "priority1", "reporter1", "assignee1",
+                        new Date(), new Date()),
+                new BugReport("title2", "description2", "status2", "severity2", "priority2", "reporter2", "assignee2",
+                        new Date(), new Date()));
 
         when(bugReportService.getAllBugReports()).thenReturn(bugReports);
 
         String bugReportsAsJson = mapper.writeValueAsString(bugReports);
 
-        mockMvc.perform(get("/bug-reports/")).andExpectAll(status().isOk(), content().json(bugReportsAsJson, false));
+        mockMvc.perform(get(BUG_REPORT_CONTROLLER_URI + "/")).andExpectAll(status().isOk(), content().json(bugReportsAsJson, false));
+    }
+
+    @Test
+    void createNewBugReportShouldAddToListOfBugReports() throws Exception {
+
+        String bugReportToAddAsJson = mapper.writeValueAsString(new BugReport("title3", "description3",
+                "status3", "severity3", "priority3", "reporter3", "assignee3", new Date(), new Date()));
+
+        mockMvc.perform(post(BUG_REPORT_CONTROLLER_URI + "/").contentType(MediaType.APPLICATION_JSON)
+                .content(bugReportToAddAsJson)).andExpect(status().isOk());
+
     }
 }
