75e2b8bd4c4dda512bbbb11fb0287a0d0a1b9bbc
gaatvul <kungfuscience@hotmail.com>
2023-06-25 21:23:53 +0200

    Added validation on fields vital for a barebones bug report.

--- bug-reporting-service/src/main/java/com/lduff/bugreportingservice/models/BugReport.java
+++ bug-reporting-service/src/main/java/com/lduff/bugreportingservice/models/BugReport.java
@@ -2,6 +2,10 @@ package com.lduff.bugreportingservice.models;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +15,26 @@ import lombok.NoArgsConstructor;
 @Data
 public class BugReport {
 
+    @NotEmpty(message = "Title cannot be blank.")
     private String title;
+
+    @NotEmpty(message = "Description cannot be blank.")
     private String description;
+
     private String status;
+
     private String severity;
+
     private String priority;
+
+    @NotEmpty(message = "Reporter cannot be blank.")
     private String reporter;
+
     private String assignee;
+
+    @NotEmpty(message = "Reported On must not be blank.")
     private Date reportedOn;
+
     private Date lastUpdated;
 
 }
